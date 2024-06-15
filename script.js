78ae65e284f8441c53779d6c0a017affa4147f12
rforraden <158191408+rforraden@users.noreply.github.com>
2024-06-15 22:35:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+sconst audio = document.querySelector('audio');
+const body = document.body;
+
+audio.addEventListener('play', () => {
+  body.classList.add('play-music');
+});
+
+audio.addEventListener('pause', () => {
+  body.classList.remove('play-music');
+});
